703453ce203028ec77372622abddf73b5b16e7c2
Almir Sharifullin <a.sharifullin@tools4brokers.com>
2020-07-19 13:21:39 +0300

    Add project files.

---
+++ netcore-mock-server/Controllers/ApplicationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace netcore_mock_server.Controllers
+{
+    [ApiController]
+    [Route("/api")]
+    public class ApplicationController : ControllerBase
+    {
+        private readonly ILogger<ApplicationController> _logger;
+
+        public ApplicationController(ILogger<ApplicationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            return null;
+        }
+    }
+}
