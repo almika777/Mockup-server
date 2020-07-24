4e3cf6781ec3adafb9457d2d7926966c0029b085
almika777 <47299790+almika777@users.noreply.github.com>
2020-07-24 17:26:41 +0300

    Add custom config object for app. Register him as singletone. Change default used ports (#1)
    * Add configuration for app. Register as singletone

---
+++ netcore-mock-server/Configuration/ApplicationConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace netcore_mock_server.Configuration
+{
+    public enum ApplicationMode
+    {
+        [EnumMember(Value = "File")]
+        File,
+        [EnumMember(Value = "Files")]
+        Files,
+        [EnumMember(Value = "Dirrectory")]
+        Dirrectory
+    }
+
+    public class ApplicationConfig : IApplicationConfig
+    {
+        public ApplicationMode ApplicationMode { get; set; }
+        public string PathToRootFolder { get; set; }
+    }
+}
---
+++ netcore-mock-server/Configuration/IApplicationConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace netcore_mock_server.Configuration
+{
+    public interface IApplicationConfig
+    {
+        ApplicationMode ApplicationMode { get; set; }
+        string PathToRootFolder { get; set; }
+    }
+}
